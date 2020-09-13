3d442e36f999fbfe71a2e69016aee9b10e589400
liveforensics <me@here.com>
2020-09-13 13:42:27 +0100

    wesd

--- Windows/LocalBuild/build1.ps1
+++ Windows/LocalBuild/build1.ps1
@@ -1,4 +1,4 @@
 Write-Host 'Installing Build Tools from local layout'
 Set-Location 'c:\tools'
-Get-ChildItem .
+.\vs_buildtools.exe --quiet --wait --norestart --nocache --add Microsoft.VisualStudio.Workload.MSBuildTools --installPath C:\BuildTools
 
