165b70745f5f20b9cb87d2620ad0756fd2877e7a
liveforensics <me@here.com>
2020-10-02 18:25:15 +0100

    cxzcxz

--- Containers/buildtools2019/step1/build.ps1
+++ Containers/buildtools2019/step1/build.ps1
@@ -10,6 +10,7 @@ Set-Location C:\vs
     --add Microsoft.VisualStudio.Workload.WebBuildTools `
     --add Microsoft.VisualStudio.Workload.MSBuildTools `
     --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
-    --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
+    --add Microsoft.VisualStudio.Component.VC.v141.x86.x64 `
+    --add Microsoft.VisualStudio.Workload.NetCoreBuildTools
 
 
