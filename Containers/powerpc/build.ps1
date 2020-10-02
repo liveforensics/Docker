392322b591291b79422dd0e45f5f6d5c66f29dec
liveforensics <me@here.com>
2020-10-02 14:58:04 +0100

    bvcv

--- Containers/powerpc/build.ps1
+++ Containers/powerpc/build.ps1
@@ -3,7 +3,7 @@
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.8.6/python-3.8.6-embed-amd64.zip" -OutFile "c:/temp/python-3.8.6-embed-amd64.zip"
 Expand-Archive -Path 'c:/temp/python-3.8.6-embed-amd64.zip' -DestinationPath 'c:\python3'
 
-Expand-Archive -Path 'c:\temp\powerpc-eabi.zip' -DestinationPath 'c:\powerpc-eabi'
+Expand-Archive -Path 'c:\temp\powerpc-eabi.zip' -DestinationPath 'c:\'
 Remove-Item c:\temp\powerpc-eabi.zip -Force
 
 $CurrentPath = (Get-Itemproperty -path 'hklm:\system\currentcontrolset\control\session manager\environment' -Name Path).Path
