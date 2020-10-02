667bb0a79807fd8b73fb9bd904215fe680e9db84
liveforensics <me@here.com>
2020-10-02 14:51:46 +0100

    xcxvcx

--- Containers/powerpc/build.ps1
+++ Containers/powerpc/build.ps1
@@ -3,9 +3,9 @@
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.8.6/python-3.8.6-embed-amd64.zip" -OutFile "c:/temp/python-3.8.6-embed-amd64.zip"
 Expand-Archive -Path 'c:/temp/python-3.8.6-embed-amd64.zip' -DestinationPath 'c:\python3'
 
-Invoke-WebRequest -Uri "http://sysprogs.com/getfile/34/powerpc-eabi-gcc4.9.0.exe" -OutFile "c:/temp/powerpc.exe"
-
+Expand-Archive -Path 'c:\temp\powerpc-eabi.zip' -DestinationPath 'c:\powerpc-eabi'
+Remove-Item c:\temp\powerpc-eabi.zip -Force
 
 $CurrentPath = (Get-Itemproperty -path 'hklm:\system\currentcontrolset\control\session manager\environment' -Name Path).Path
-$NewPath = $CurrentPath + ';C:\python3'
+$NewPath = $CurrentPath + ';C:\python3;c:\powerpc-eabi\bin'
 Set-ItemProperty -path 'hklm:\system\currentcontrolset\control\session manager\environment' -Name Path -Value $NewPath
