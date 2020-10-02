3e435846eb278102ad5b5d0b3958b2738b69a026
liveforensics <me@here.com>
2020-10-02 15:18:34 +0100

    vcx

--- Swarm/Builder/start.sh
+++ Swarm/Builder/start.sh
@@ -1,1 +1,6 @@
+docker pull jenkins/jenkins:latest
+docker pull liveforensics/buildbox:2004-powerpc
+docker pull liveforensics/buildbox:2004-buildtools2019
+docker pull sonatype/nexus3
+
 docker stack deploy --compose-file=build-stack.yml builder
