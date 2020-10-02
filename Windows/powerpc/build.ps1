08063633a40a1f8381845ad8d6a99523e3090901
liveforensics <me@here.com>
2020-10-02 09:05:02 +0100

    gfd

--- Windows/powerpc/build.ps1
+++ Windows/powerpc/build.ps1
@@ -1,5 +1,8 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/temp/python-3.7.0.exe"
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.8.6/python-3.8.6-embed-amd64.zip" -OutFile "c:/temp/python-3.8.6-embed-amd64.zip"
+Expand-Archive -Path 'c:/temp/python-3.8.6-embed-amd64.zip' -DestinationPath 'c:\python3'
 
-c:/temp/python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+$CurrentPath = (Get-Itemproperty -path 'hklm:\system\currentcontrolset\control\session manager\environment' -Name Path).Path
+$NewPath = $CurrentPath + ';C:\python3'
+Set-ItemProperty -path 'hklm:\system\currentcontrolset\control\session manager\environment' -Name Path -Value $NewPath
