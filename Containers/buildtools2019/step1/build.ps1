4945f2d1c5a55df1dc319f0cad9bab7fce6ac95b
liveforensics <me@here.com>
2020-10-02 12:09:52 +0100

    vcx

--- Containers/buildtools2019/step1/build.ps1
+++ Containers/buildtools2019/step1/build.ps1
@@ -2,4 +2,4 @@ write-host 'Installing Build Tools'
 
 Set-Location C:\vs
 
-vs_buildtools.exe --quiet --wait --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
+.\vs_buildtools.exe --quiet --wait --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
