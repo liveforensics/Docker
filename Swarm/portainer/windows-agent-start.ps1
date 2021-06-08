d24a0307aa3a19dac4f97c55c4f5c95a8c50b8bf
liveforensics <liveforensics@gmail.com>
2021-06-08 12:12:51 +0100

    fdsf

--- Swarm/portainer/windows-agent-start.ps1
+++ Swarm/portainer/windows-agent-start.ps1
@@ -1,4 +1,5 @@
     docker pull portainer:agent:latest
+    docker kill portainer_agent
     docker rm portainer_agent
     
     docker run -d --name portainer_agent --restart always --network testlab-network -e AGENT_CLUSTER_ADDR=tasks.portainer_agent --mount type=npipe,source=\\.\pipe\docker_engine,target=\\.\pipe\docker_engine portainer/agent:latest
