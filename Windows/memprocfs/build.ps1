6f3e5c09abd40ac1f49da48eb4b298f8a0e1068e
liveforensics <liveforensicsuk@gmail.com>
2022-08-17 17:33:48 +0100

    memprocfs added

---
+++ Windows/memprocfs/build.ps1
@@ -0,0 +1,41 @@
+Write-Host "Installing chocolatey"
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Write-Host "Downloading QT"
+# $downloadUrl = "http://download.qt.io/official_releases/qt/5.12/5.12.9/qt-opensource-windows-x86-5.12.9.exe"
+# Invoke-WebRequest -Uri $downloadUrl -OutFile c:\temp\qt.exe
+
+Write-Host "Installing Dokan"
+Start-Process c:\temp\Dokan_x64.msi -ArgumentList "/quiet"
+
+Write-Host "Installing Python 3"
+choco install -y python3
+
+Expand-Archive -Path 'c:/temp/memproc.zip' -DestinationPath 'c:\memprocfs'
+
+
+
+# Write-Host "Installing Perl - eek!"
+# choco install -y activeperl
+
+# Write-Host "Installing git"
+# choco.exe install -y git
+
+# Write-Host "Installing Java"
+# choco install -y openjdk11
+
+# Write-Host "Installing CMake"
+# choco install -y cmake
+
+# Write-Host "Installing Asciidoctor"
+# choco install -y asciidoctorj xsltproc
+
+# Write-Host "Installing winflexbison"
+# choco install -y winflexbison3
+
+# Write-Host "Creating development folder"
+# New-Item -ItemType Directory -Path c:\Development
+# New-Item -ItemType Directory -Path c:\Development\wsbuild64
+
+# Write-Host "Installing vs community edition"
+# choco install -y visualstudio2019community visualstudio2019-workload-nativedesktop
